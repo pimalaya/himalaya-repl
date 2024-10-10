2c431a9c9d915c890f4dd1ccd57350bd9305430d
Clément DOUIN <clement.douin@posteo.net>
2024-10-10 08:07:24 +0200

    update nix flakes, gh workflows and docs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,15 @@
   description = "REPL to manage emails";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     gitignore = {
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     fenix = {
-      url = "github:nix-community/fenix";
+      # https://github.com/nix-community/fenix/pull/145
+      # url = "github:nix-community/fenix";
+      url = "github:soywod/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     naersk = {
@@ -23,132 +25,210 @@
 
   outputs = { self, nixpkgs, gitignore, fenix, naersk, ... }:
     let
+      inherit (nixpkgs) lib;
       inherit (gitignore.lib) gitignoreSource;
+
+      crossSystems = {
+        x86_64-linux = {
+          x86_64-linux = {
+            rustTarget = "x86_64-unknown-linux-musl";
+          };
+
+          aarch64-linux = rec {
+            rustTarget = "aarch64-unknown-linux-musl";
+            runner = { pkgs, himalaya }: "${pkgs.qemu}/bin/qemu-aarch64 ${himalaya}";
+            mkPackage = { system, pkgs }: package:
+              let
+                inherit (mkPkgsCross system rustTarget) stdenv;
+                cc = "${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc";
+              in
+              package // {
+                TARGET_CC = cc;
+                CARGO_BUILD_RUSTFLAGS = package.CARGO_BUILD_RUSTFLAGS ++ [ "-Clinker=${cc}" ];
+              };
+          };
+
+          x86_64-windows = {
+            rustTarget = "x86_64-pc-windows-gnu";
+            runner = { pkgs, himalaya }:
+              let wine = pkgs.wine.override { wineBuild = "wine64"; };
+              in "${wine}/bin/wine64 ${himalaya}.exe";
+            mkPackage = { system, pkgs }: package:
+              let
+                inherit (pkgs.pkgsCross.mingwW64) stdenv windows;
+                cc = "${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc";
+              in
+              package // {
+                depsBuildBuild = [ stdenv.cc windows.pthreads ];
+                TARGET_CC = cc;
+                CARGO_BUILD_RUSTFLAGS = package.CARGO_BUILD_RUSTFLAGS ++ [ "-Clinker=${cc}" ];
+              };
+          };
+        };
+
+        aarch64-linux = {
+          aarch64-linux = {
+            rustTarget = "aarch64-unknown-linux-musl";
+          };
+        };
+
+        x86_64-darwin = {
+          x86_64-darwin = {
+            rustTarget = "x86_64-apple-darwin";
+            mkPackage = { pkgs, ... }: package:
+              let inherit (pkgs.darwin.apple_sdk.frameworks) AppKit Cocoa;
+              in
+              package // {
+                buildInputs = [ Cocoa ];
+                NIX_LDFLAGS = "-F${AppKit}/Library/Frameworks -framework AppKit";
+              };
+          };
+
+          # FIXME: https://github.com/NixOS/nixpkgs/issues/273442
+          aarch64-darwin = {
+            rustTarget = "aarch64-apple-darwin";
+            runner = { pkgs, himalaya }: "${pkgs.qemu}/bin/qemu-aarch64 ${himalaya}";
+            mkPackage = { system, pkgs }: package:
+              let
+                inherit ((mkPkgsCross system "aarch64-darwin").pkgsStatic) stdenv darwin;
+                inherit (darwin.apple_sdk.frameworks) AppKit Cocoa;
+                cc = "${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc";
+              in
+              package // {
+                buildInputs = [ Cocoa ];
+                NIX_LDFLAGS = "-F${AppKit}/Library/Frameworks -framework AppKit";
+                TARGET_CC = cc;
+                CARGO_BUILD_RUSTFLAGS = package.CARGO_BUILD_RUSTFLAGS ++ [ "-Clinker=${cc}" ];
+              };
+          };
+        };
+
+        aarch64-darwin = {
+          aarch64-darwin = {
+            rustTarget = "aarch64-apple-darwin";
+            mkPackage = { pkgs, ... }: package:
+              let inherit (pkgs.darwin.apple_sdk.frameworks) AppKit Cocoa;
+              in
+              package // {
+                buildInputs = [ Cocoa ];
+                NIX_LDFLAGS = "-F${AppKit}/Library/Frameworks -framework AppKit";
+              };
+          };
+        };
+      };
+
+      eachBuildSystem = lib.genAttrs (builtins.attrNames crossSystems);
+
+      mkPkgsCross = buildSystem: crossSystem: import nixpkgs {
+        system = buildSystem;
+        crossSystem.config = crossSystem;
+      };
 
       mkToolchain = import ./rust-toolchain.nix fenix;
 
-      mkDevShells = buildPlatform:
+      mkApp = { pkgs, buildSystem, targetSystem ? buildSystem }:
         let
-          pkgs = import nixpkgs { system = buildPlatform; };
-          rust-toolchain = mkToolchain.fromFile { system = buildPlatform; };
+          himalaya = lib.getExe self.packages.${buildSystem}.${targetSystem};
+          wrapper = crossSystems.${buildSystem}.${targetSystem}.runner or (_: himalaya) { inherit pkgs himalaya; };
+          program = lib.getExe (pkgs.writeShellScriptBin "himalaya" "${wrapper} $@");
+          app = { inherit program; type = "app"; };
         in
-        {
-          default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-            ];
+        app;
+
+      mkApps = buildSystem:
+        let
+          pkgs = import nixpkgs { system = buildSystem; };
+          mkApp' = targetSystem: _: mkApp { inherit pkgs buildSystem targetSystem; };
+          defaultApp = mkApp { inherit pkgs buildSystem; };
+          apps = builtins.mapAttrs mkApp' crossSystems.${buildSystem};
+        in
+        apps // { default = defaultApp; };
+
+      mkPackage = { pkgs, buildSystem, targetSystem ? buildSystem }:
+        let
+          targetConfig = crossSystems.${buildSystem}.${targetSystem};
+          toolchain = mkToolchain.fromTarget {
+            inherit pkgs buildSystem;
+            targetSystem = targetConfig.rustTarget;
+          };
+          rust = naersk.lib.${buildSystem}.override {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+          mkPackage' = targetConfig.mkPackage or (_: p: p);
+          himalaya = "./himalaya";
+          runner = targetConfig.runner or (_: himalaya) { inherit pkgs himalaya; };
+          package = mkPackage' { inherit pkgs; system = buildSystem; } {
+            name = "himalaya";
+            src = gitignoreSource ./.;
+            strictDeps = true;
+            doCheck = false;
+            auditable = false;
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            CARGO_BUILD_TARGET = targetConfig.rustTarget;
+            CARGO_BUILD_RUSTFLAGS = [ "-Ctarget-feature=+crt-static" ];
+            postInstall = ''
+              export WINEPREFIX="$(mktemp -d)"
+
+              mkdir -p $out/bin/share/{applications,completions,man,services}
+              cp assets/himalaya.desktop $out/bin/share/applications/
+              cp assets/himalaya-watch@.service $out/bin/share/services/
+
+              cd $out/bin
+              ${runner} man ./share/man
+              ${runner} completion bash > ./share/completions/himalaya.bash
+              ${runner} completion elvish > ./share/completions/himalaya.elvish
+              ${runner} completion fish > ./share/completions/himalaya.fish
+              ${runner} completion powershell > ./share/completions/himalaya.powershell
+              ${runner} completion zsh > ./share/completions/himalaya.zsh
+              tar -czf himalaya.tgz himalaya* share
+              ${pkgs.zip}/bin/zip -r himalaya.zip himalaya* share
+
+              mv share ../
+              mv himalaya.tgz himalaya.zip ../
+            '';
+          };
+        in
+        rust.buildPackage package;
+
+      mkPackages = buildSystem:
+        let
+          pkgs = import nixpkgs { system = buildSystem; };
+          mkPackage' = targetSystem: _: mkPackage { inherit pkgs buildSystem targetSystem; };
+          defaultPackage = mkPackage { inherit pkgs buildSystem; };
+          packages = builtins.mapAttrs mkPackage' crossSystems.${buildSystem};
+        in
+        packages // { default = defaultPackage; };
+
+      mkDevShells = buildSystem:
+        let
+          pkgs = import nixpkgs { system = buildSystem; };
+          rust-toolchain = mkToolchain.fromFile { inherit buildSystem; };
+          defaultShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ pkg-config ];
             buildInputs = with pkgs; [
               # Nix
-              rnix-lsp
+              nixd
               nixpkgs-fmt
 
               # Rust
               rust-toolchain
               cargo-watch
 
-              # OpenSSL
-              openssl.dev
-
-              # Notmuch
-              notmuch
-
-              # GPG
+              # Email env
               gnupg
               gpgme
+              msmtp
+              notmuch
             ];
           };
-        };
-
-      mkPackage = pkgs: buildPlatform: targetPlatform: package:
-        let
-          toolchain = mkToolchain.fromTarget {
-            inherit pkgs buildPlatform targetPlatform;
-          };
-          naersk' = naersk.lib.${buildPlatform}.override {
-            cargo = toolchain;
-            rustc = toolchain;
-          };
-          package' = {
-            name = "himalaya-repl";
-            src = gitignoreSource ./.;
-            overrideMain = _: {
-              postInstall = ''
-                mkdir -p $out/share/applications/
-                cp assets/himalaya-repl.desktop $out/share/applications/
-              '';
-            };
-            doCheck = true;
-            cargoTestOptions = opts: opts ++ [ "--lib" ];
-          } // pkgs.lib.optionalAttrs (!isNull targetPlatform) {
-            CARGO_BUILD_TARGET = targetPlatform;
-          } // package;
         in
-        naersk'.buildPackage package';
+        { default = defaultShell; };
 
-      mkPackages = buildPlatform:
-        let
-          pkgs = import nixpkgs { system = buildPlatform; };
-          mkPackage' = mkPackage pkgs buildPlatform;
-        in
-        rec {
-          default = if pkgs.stdenv.isDarwin then macos else linux;
-          linux = mkPackage' null { };
-          linux-musl = mkPackage' "x86_64-unknown-linux-musl" (with pkgs.pkgsStatic; {
-            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-            hardeningDisable = [ "all" ];
-          });
-          macos = mkPackage' null (with pkgs.darwin.apple_sdk.frameworks; {
-            # NOTE: needed to prevent error Undefined symbols
-            # "_OBJC_CLASS_$_NSImage" and
-            # "_LSCopyApplicationURLsForBundleIdentifier"
-            NIX_LDFLAGS = "-F${AppKit}/Library/Frameworks -framework AppKit";
-            buildInputs = [ Cocoa ];
-          });
-          # FIXME: bzlip: fatal error: windows.h: No such file or directory
-          # May be related to SQLite.
-          windows = mkPackage' "x86_64-pc-windows-gnu" {
-            strictDeps = true;
-            depsBuildBuild = with pkgs.pkgsCross.mingwW64; [
-              stdenv.cc
-              windows.pthreads
-            ];
-          };
-        };
-
-      mkApp = drv:
-        let exePath = drv.passthru.exePath or "/bin/himalaya-repl";
-        in
-        {
-          type = "app";
-          program = "${drv}${exePath}";
-        };
-
-      mkApps = buildPlatform:
-        let
-          pkgs = import nixpkgs { system = buildPlatform; };
-        in
-        rec {
-          default = if pkgs.stdenv.isDarwin then macos else linux;
-          linux = mkApp self.packages.${buildPlatform}.linux;
-          linux-musl = mkApp self.packages.${buildPlatform}.linux-musl;
-          macos = mkApp self.packages.${buildPlatform}.macos;
-          windows =
-            let
-              wine = pkgs.wine.override { wineBuild = "wine64"; };
-              himalaya = self.packages.${buildPlatform}.windows;
-              app = pkgs.writeShellScriptBin "himalaya" ''
-                export WINEPREFIX="$(mktemp -d)"
-                ${wine}/bin/wine64 ${himalaya}/bin/himalaya.exe $@
-              '';
-            in
-            mkApp app;
-        };
-      supportedSystems = [ "aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems f;
     in
     {
-      apps = forEachSupportedSystem mkApps;
-      packages = forEachSupportedSystem mkPackages;
-      devShells = forEachSupportedSystem mkDevShells;
+      apps = eachBuildSystem mkApps;
+      packages = eachBuildSystem mkPackages;
+      devShells = eachBuildSystem mkDevShells;
     };
 }
